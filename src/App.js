b4040019901c77bc2e5e911690f795678cdd0f79
daklo91 <coffeedog91@gmail.com>
2021-11-23 16:52:42 +0100

    Add contents from custom hook to App.js Send theme and changeTheme as props to Header

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
 import Footer from "./components/Layout/Footer";
 import Header from "./components/Layout/Header";
-import useThemeSwitch from "./hooks/use-theme-switch";
+import { useState, useEffect } from "react";
 
 function App() {
-  const theme = useThemeSwitch();
+  const [theme, setTheme] = useState(
+    //if storage empty, run code
+    window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light"
+  );
+
+  useEffect(() => {
+    //add if ("has theme in storage"). if it has, dont run code.
+    const watchTheme = window
+      .matchMedia("(prefers-color-scheme: dark)")
+      .addEventListener("change", (e) => {
+        const newTheme = e.matches ? "dark" : "light";
+        setTheme(newTheme);
+      });
+    return () => {
+      window.removeEventListener("change", watchTheme);
+    };
+  }, []);
+
+  const changeTheme = (themeString) => {
+    setTheme(themeString);
+    //save to storage
+  };
 
   return (
     <div id="theme-switch" data-theme={theme}>
       <div id="calculator-body">
-        <Header />
+        <Header changeTheme={changeTheme} theme={theme} />
         <main></main>
       </div>
       <Footer />
