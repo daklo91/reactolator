5db4359a5da113cf9f4eecae019476af30a02d32
daklo91 <coffeedog91@gmail.com>
2021-11-23 14:53:45 +0100

    Switch transitions

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,6 +1,24 @@
+import { useState } from "react";
 import classes from "./Header.module.css";
 
 const Header = () => {
+  const [switchTheme, setSwitchTheme] = useState(1);
+
+  const toggleSwitch = () => {
+    if (switchTheme >= 3) {
+      setSwitchTheme(1);
+    } else setSwitchTheme(switchTheme + 1);
+  };
+
+  const toggleSwitchNotch =
+    switchTheme === 1
+      ? { marginLeft: "0px" }
+      : switchTheme === 2
+      ? { marginLeft: "22.5px" }
+      : { marginLeft: "45px" };
+
+  console.log(toggleSwitchNotch);
+
   return (
     <header>
       <h1>calc</h1>
@@ -8,8 +26,11 @@ const Header = () => {
         <span className={classes["switch-numbers"]}>123</span>
         <div className={classes["switch-container--row"]}>
           <span>THEME</span>
-          <div className={classes["switch-body"]}>
-            <div className={classes["switch-knob"]}></div>
+          <div className={classes["switch-body"]} onClick={toggleSwitch}>
+            <div
+              className={classes["switch-knob"]}
+              style={toggleSwitchNotch}
+            ></div>
           </div>
         </div>
       </div>
