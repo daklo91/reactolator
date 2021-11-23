2e4456199a0d150bafc69388d705983e42839d09
daklo91 <coffeedog91@gmail.com>
2021-11-23 16:54:02 +0100

    Add change theme depending on toggle number Add useEffect to two-way bind the switch and theme

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,13 +1,30 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import classes from "./Header.module.css";
 
-const Header = () => {
+const Header = (props) => {
   const [switchTheme, setSwitchTheme] = useState(1);
 
-  const toggleSwitch = () => {
-    if (switchTheme >= 3) {
+  useEffect(() => {
+    if (props.theme === "dark") {
       setSwitchTheme(1);
-    } else setSwitchTheme(switchTheme + 1);
+    } else if (props.theme === "light") {
+      setSwitchTheme(2);
+    } else if (props.theme === "purple") {
+      setSwitchTheme(3);
+    }
+  }, [props.theme]);
+
+  const toggleSwitch = () => {
+    if (switchTheme === 3) {
+      setSwitchTheme(1);
+      props.changeTheme("dark");
+    } else if (switchTheme === 1) {
+      setSwitchTheme(2);
+      props.changeTheme("light");
+    } else if (switchTheme === 2) {
+      setSwitchTheme(3);
+      props.changeTheme("purple");
+    }
   };
 
   const toggleSwitchNotch =
