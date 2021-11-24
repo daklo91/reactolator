2065479861f269f0badfda501e950319166ef74a
daklo91 <coffeedog91@gmail.com>
2021-11-24 12:52:07 +0100

    Create display component and css module

---
+++ src/components/calculator/display.js
@@ -0,0 +1,12 @@
+import classes from "./display.module.css";
+
+const display = () => {
+  return (
+    <div className={classes.display}>
+      {/* placeholder text will recieve props */}
+      399,981
+    </div>
+  );
+};
+
+export default display;
