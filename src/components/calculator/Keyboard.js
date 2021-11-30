f0dec1e89545b27dbf3d2993bd288b5d2245c2c1
daklo91 <coffeedog91@gmail.com>
2021-11-30 20:39:32 +0100

    Update symbolArray with more infro on symbols Update keypress to be smaller and manageable Update useEffect. moved it after keyPress definition

--- src/components/calculator/Keyboard.js
+++ src/components/calculator/Keyboard.js
@@ -1,32 +1,45 @@
 import { useEffect, useState } from "react";
 import classes from "./Keyboard.module.css";
 
-const symbolArray = [
-  "7",
-  "8",
-  "9",
-  "DEL",
-  "4",
-  "5",
-  "6",
-  "+",
-  "1",
-  "2",
-  "3",
-  "-",
-  ".",
-  "0",
-  "/",
-  "x",
-  "RESET",
-  "=",
+const symbols = [
+  { name: "7", key: ["7"], type: "number" },
+  { name: "8", key: ["8"], type: "number" },
+  { name: "9", key: ["9"], type: "number" },
+  { name: "DEL", key: ["Backspace"], type: "other" },
+  { name: "4", key: ["4"], type: "number" },
+  { name: "5", key: ["5"], type: "number" },
+  { name: "6", key: ["6"], type: "number" },
+  { name: "+", key: ["+"], type: "operator" },
+  { name: "1", key: ["1"], type: "number" },
+  { name: "2", key: ["2"], type: "number" },
+  { name: "3", key: ["3"], type: "number" },
+  { name: "-", key: ["-"], type: "operator" },
+  { name: ".", key: [".", ","], type: "number" },
+  { name: "0", key: ["0"], type: "number" },
+  { name: "/", key: ["/"], type: "operator" },
+  { name: "x", key: ["x", "*"], type: "operator" },
+  { name: "RESET", key: ["Delete"], type: "other" },
+  { name: "=", key: ["Enter"], type: "other" },
 ];
 
 const Keyboard = (props) => {
   const [keyPress, setKeyPress] = useState("");
-  const getSymbol = props.getSymbol;
   const getSymbolHandler = (symbol) => {
-    getSymbol(symbol);
+    props.getSymbol(symbol);
+  };
+
+  const keyPressed = (e) => {
+    const foundSymbol = symbols.find(
+      (symbol) => e.key === symbol.key.find((key) => e.key === key)
+    );
+    if (foundSymbol) {
+      setKeyPress(foundSymbol.name);
+      getSymbolHandler(foundSymbol);
+    }
+  };
+
+  const keyReleased = () => {
+    setKeyPress("");
   };
 
   useEffect(() => {
@@ -36,92 +49,19 @@ const Keyboard = (props) => {
       document.removeEventListener("keydown", keyPressed);
       document.removeEventListener("keyup", keyReleased);
     };
-  }, []);
-
-  useEffect(() => {
-    if (keyPress) {
-      getSymbol(keyPress);
-    }
-  }, [keyPress, getSymbol]);
-
-  const keyPressed = (e) => {
-    if (e.key === "1") {
-      setKeyPress("1");
-    }
-    if (e.key === "2") {
-      setKeyPress("2");
-    }
-    if (e.key === "3") {
-      setKeyPress("3");
-    }
-    if (e.key === "4") {
-      setKeyPress("4");
-    }
-    if (e.key === "5") {
-      setKeyPress("5");
-    }
-    if (e.key === "6") {
-      setKeyPress("6");
-    }
-    if (e.key === "7") {
-      setKeyPress("7");
-    }
-    if (e.key === "8") {
-      setKeyPress("8");
-    }
-    if (e.key === "9") {
-      setKeyPress("9");
-    }
-    if (e.key === "0") {
-      setKeyPress("0");
-    }
-    if (e.key === ",") {
-      setKeyPress(".");
-    }
-    if (e.key === ".") {
-      setKeyPress(".");
-    }
-    if (e.key === "/") {
-      setKeyPress(" / ");
-    }
-    if (e.key === "x") {
-      setKeyPress(" x ");
-    }
-    if (e.key === "*") {
-      setKeyPress(" x ");
-    }
-    if (e.key === "-") {
-      setKeyPress(" - ");
-    }
-    if (e.key === "+") {
-      setKeyPress(" + ");
-    }
-    if (e.key === "Backspace") {
-      setKeyPress("DEL");
-    }
-    if (e.key === "Delete") {
-      setKeyPress("RESET");
-    }
-    if (e.key === "Enter") {
-      setKeyPress("=");
-    }
-  };
-
-  const keyReleased = () => {
-    setKeyPress("");
-  };
+  });
 
   return (
     <div className={classes.keyboard}>
-      {symbolArray.map((symbol) => {
+      {symbols.map((symbol) => {
         return (
           <button
-            className={keyPress === symbol ? classes.active : ""}
-            id={symbol}
-            key={symbol}
+            className={keyPress === symbol.name ? classes.active : ""}
+            id={symbol.name}
+            key={symbol.name}
             onClick={() => getSymbolHandler(symbol)}
           >
-            {symbol}
+            {symbol.name}
           </button>
         );
       })}
