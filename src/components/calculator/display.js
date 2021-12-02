f9051e432426c2e1f805ced76f3ec059c5e33b1c
daklo91 <coffeedog91@gmail.com>
2021-12-02 11:23:14 +0100

    Add rounding function

--- src/components/calculator/display.js
+++ src/components/calculator/display.js
@@ -1,33 +1,18 @@
 import { Fragment } from "react";
 import classes from "./Display.module.css";
 
-// const calculate = (firstNumber, operator, secondNumber) => {
-//   const op = operator.trim();
-//   if (op === "+") {
-//     return firstNumber + secondNumber;
-//   }
-//   if (op === "-") {
-//     return firstNumber - secondNumber;
-//   }
-//   if (op === "x") {
-//     return firstNumber * secondNumber;
-//   }
-//   if (op === "/") {
-//     return firstNumber / secondNumber;
-//   }
-// };
+const roundToTwo = (num) => {
+  return +(Math.round(num + "e+2") + "e-2");
+};
 
 const Display = (props) => {
   return (
     <Fragment>
       <div className={classes.display}>
-        {/* <span className={classes["text--calculation"]}>
-          &#8205;{props.savedNumber} {props.operatorSymbol}
-        </span> */}
         <span className={classes["text--result"]}>
           {props.savedNumber} {props.operatorSymbol} {props.activeNumber}{" "}
           {props.resultNumber || props.resultNumber === 0
-            ? "= " + props.resultNumber
+            ? "= " + roundToTwo(props.resultNumber)
             : null}
         </span>
       </div>
